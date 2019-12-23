2b5b7a8f26de77101d49cb62aa67703673cf94de
TAiniko <taisuke.sum@gmail.com>
2019-12-23 13:20:35 +0900

    Initial commit

---
+++ app/src/main/java/com/tainiko/androidmaterialsumple/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.tainiko.androidmaterialsumple
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.MenuItem
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        bottomNavigationView.inflateMenu(R.menu.bottom_navigation_menu)
+
+        bottomNavigationView.setOnNavigationItemSelectedListener { item: MenuItem ->
+            return@setOnNavigationItemSelectedListener when (item.itemId) {
+                R.id.bottomNavigationAlarmMenuId -> {
+                    toast("Alarm item click")
+                    true
+                }
+                R.id.bottomNavigationClockMenuId -> {
+                    toast("Clock item clicked")
+                    true
+                }
+                R.id.bottomNavigationTimerMenuId -> {
+                    toast("Timer item clicked")
+                    true
+                }
+                R.id.bottomNavigationStopWatchMenuId -> {
+                    toast("stop-watch item clicked")
+                    true
+                }
+                else -> false
+            }
+        }
+    }
+}
